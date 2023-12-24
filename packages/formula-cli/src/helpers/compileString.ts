ae07a71943183d3ee3c3dc82fda27a0e321ab88d
qrai <sergeikab77@gmail.com>
2023-12-24 19:59:14 +0400

    Added more exports

--- packages/formula-cli/src/helpers/compileString.ts
+++ packages/formula-cli/src/helpers/compileString.ts
@@ -1,23 +1,21 @@
-import Lexer from "../parser/Lexer";
-import Parser from "../parser/Parser";
 import StatementContext from "../parser/statement/StatementContext";
-import Token from "../parser/type/Token";
+import { Lexer, Parser, Token } from "@datasco/formula";
 
 export type CompileResult = {
-	result: string
-	context: StatementContext
+    result: string;
+    context: StatementContext;
 };
 
 export const compileString = (formula: string): CompileResult => {
-	// 1. Lexing
-	let lexed: Token[] = new Lexer().lex(formula);
-	
-	// 2. Parsing
-	let parseResult = new Parser().parse(lexed);
+    // 1. Lexing
+    let lexed: Token[] = new Lexer().lex(formula);
 
-	// 3. Serializing to RegExp
-	return {
-		result: parseResult.program.serialize(parseResult.context),
-		context: parseResult.context
-	};
+    // 2. Parsing
+    let parseResult = new Parser().parse(lexed);
+
+    // 3. Serializing to RegExp
+    return {
+        result: parseResult.program.serialize(parseResult.context),
+        context: parseResult.context,
+    };
 };
--- packages/formula/src/index.ts
+++ packages/formula/src/index.ts
@@ -1,5 +1,10 @@
 import Lexer from "./parser/Lexer";
 import Parser from "./parser/Parser";
 import Token from "./parser/type/Token";
+import TokenType from "./parser/type/TokenType";
+import StatementContext from "./parser/statement/StatementContext";
+import StatementType from "./parser/statement/StatementType";
+import IStatement from "./parser/statement/IStatement";
+import IParentStatement from "./parser/statement/IParentStatement";
 
-export { Lexer, Parser, Token };
+export { Lexer, Parser, Token, TokenType, StatementContext, StatementType, IStatement, IParentStatement };
